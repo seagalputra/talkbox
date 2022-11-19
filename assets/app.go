cfa69526683f59a2c6ce42ee9c119164f2227ad8
seagalputra <seagalputra@gmail.com>
2022-11-19 14:18:26 +0700

    feat: setup golang project

---
+++ assets/app.go
@@ -0,0 +1,38 @@
+package assets
+
+import (
+	"os"
+	"reflect"
+
+	"github.com/joho/godotenv"
+)
+
+type (
+	appConfig struct {
+		Port       string `env:"PORT"`
+		MongoDBURI string `env:"MONGODB_URI"`
+	}
+)
+
+var AppConfig appConfig
+
+const tagName = "env"
+
+func LoadAppConfig() error {
+	err := godotenv.Load()
+	if err != nil {
+		return err
+	}
+
+	AppConfig = appConfig{}
+	t := reflect.TypeOf(AppConfig)
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		tag := field.Tag.Get(tagName)
+		osEnv := os.Getenv(tag)
+		reflect.ValueOf(&AppConfig).Elem().FieldByName(field.Name).SetString(osEnv)
+	}
+
+	return nil
+}
