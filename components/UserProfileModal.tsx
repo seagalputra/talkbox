f34c9715717c6bc254dccdd47239a60714d3fc67
seagalputra <seagalputra@gmail.com>
2022-12-15 06:24:48 +0700

    feat: create avatar edit modal

--- components/UserProfileModal.tsx
+++ components/UserProfileModal.tsx
@@ -35,12 +35,35 @@ const UserProfileModal = ({ openUserProfileModal }: any) => {
           </button>
         </div>
 
-        <div className="flex justify-center mt-4">
+        <div className="mt-4 relative flex justify-center">
           <img
-            className="rounded-full w-24 border-2 border-slate-100"
+            className="rounded-full w-24"
             src="https://i.picsum.photos/id/524/200/200.jpg?hmac=t6LNfKKZ41wUVh8ktcFHag3CGQDzovGpZquMO5cbH-o"
             alt="User avatar"
           />
+
+          <div className="rounded-full w-24 absolute top-0 w-full h-full flex items-center justify-center text-transparent hover:bg-black/[0.4] hover:text-white transition duration-200">
+            <input type="file" className="opacity-0 absolute" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z"
+              />
+            </svg>
+          </div>
         </div>
         <form className="mt-4 grid grid-cols-2 gap-6">
           <div className="col-span-1">
